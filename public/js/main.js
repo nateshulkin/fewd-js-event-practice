5ac7f2be394f4a76ea8753f102f699813bd99147
Nathan Shulkin <nateshulkin@gmail.com>
2018-11-06 18:53:09 -0500

    build javascript

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,14 @@
-(function () {
+const buttons = document.querySelectorAll('button');
+const button1 = buttons[0];
+const button2 = buttons[1];
+const button3 = buttons[2];
+button1.addEventListener('click', function() {
+  button1.classList.toggle('button-danger');
+});
 
-  // your js goes here
-
-})();
+button2.addEventListener('click', function() {
+  button2.innerText = 'You Clicked Me!';
+});
+button3.addEventListener('click', function() {
+  button2.innerText = 'button two';
+});
